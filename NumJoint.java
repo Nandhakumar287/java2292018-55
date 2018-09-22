cbfe1da33fdac376ede0b2605c50799f461b5963
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-09-23 01:51:32 +0530

    Update and rename README.md to NumJoint.java

---
+++ NumJoint.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+  public class NumJoint{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Num1:");
+      String num1=sc.nextLine();
+      System.out.println("Enter Num1:");
+      String num2=sc.nextLine();
+      System.out.println("-->"+(num1+num2));
+      }
+    }  
